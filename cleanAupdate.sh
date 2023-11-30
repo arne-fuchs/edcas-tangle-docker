547f7c3f0d172a987b0858c10dd41d9fb189a70e
arne-fuchs <arnefuchs@protonmail.com>
2023-11-30 23:48:55 +0000

    Update

--- cleanAupdate.sh
+++ cleanAupdate.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-sudo docker container stop $(sudo docker ps -q) && sudo docker compose down && sudo ./wipeDocker.sh && sudo ./cleanup.sh && git pull && sudo docker compose pull && sudo ./bootstrap.sh && sudo ./run.sh 4 && sudo docker ps -a
+sudo docker container stop $(sudo docker ps -q)
+sudo docker compose down
+sudo ./wipeDocker.sh
+sudo ./cleanup.sh
+git pull
+sudo docker compose pull
+sudo ./bootstrap.sh
+sudo ./run.sh 4
+sudo docker ps -a
